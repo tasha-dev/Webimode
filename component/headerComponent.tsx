4b99d4571349c28c5841c5ab66b27b0cd12ca5d0
Mahdi Tasha <imwhdiiii@gmail.com>
2023-11-14 20:13:05 +0330

    added mqs of header

--- component/headerComponent.tsx
+++ component/headerComponent.tsx
@@ -11,11 +11,21 @@ import LinkComponent from "@/chunk/header/linkComponent";
 export default function HeaderComponent():ReactNode {
     // Returning JSX
     return (
-        <header className={'px-[32px] py-[36px] flex items-center justify-between gap-[10px] flex-wrap'}>
-            <div className={'flex items-center gap-[30px]'}>
-                <Image src={LogoImage.src} alt={'لوگو وبیمود'} width={56} height={56} />
-                <div className={'vertical-divider'} />
-                <ul className={'flex items-center gap-[30px]'}>
+        <header className={'px-[32px] py-[36px] lg:flex items-center justify-between gap-[10px] flex-wrap bg-white'}>
+            <div className={'flex items-center lg:justify-start justify-between lg:w-auto w-full gap-[30px]'}>
+                <button className={'lg:hidden flex aspect-square w-[50px] items-center justify-center rounded-[14px] bg-white border border-dark text-black'}>
+                    <IconComponent name={'list-right'} size={20} />
+                </button>
+                <Link href={'/'}>
+                    <Image src={LogoImage.src} alt={'لوگو وبیمود'} width={56} height={56} />
+                </Link>
+                <div className={'lg:hidden block'}>
+                    <button className={'w-[50px] aspect-square p-0 btn-primary'}>
+                        <IconComponent name={'shaking-hands'} size={20} />
+                    </button>
+                </div>
+                <div className={'vertical-divider lg:block hidden'} />
+                <ul className={'lg:flex hidden items-center gap-[30px]'}>
                     <li><LinkComponent link={'/work-samples'}>نمونه کار ها</LinkComponent></li>
                     <DropdownComponent link={'/services'} title={'خدمات ما'}>
                         <li><Link className={'px-[15px] truncate text-[16px] text-dark font-normal py-[5px] block hover:bg-white/30 transition-all duration-500'} href={'#'}>لینک ۱</Link></li>
@@ -26,12 +36,12 @@ export default function HeaderComponent():ReactNode {
                     </DropdownComponent>
                 </ul>
             </div>
-            <div className={'flex gap-[20px]'}>
+            <div className={'lg:flex hidden gap-[20px]'}>
                 <Link href={'#'} className={'btn-primary'}>
                     درخواست همکاری
                     <IconComponent name={'chevron-left'} size={15} />
                 </Link>
-                <button className={'aspect-square w-[50px] flex items-center justify-center rounded-[14px] bg-white border border-dark'}>
+                <button className={'aspect-square w-[50px] flex items-center justify-center rounded-[14px] bg-white border border-dark text-black'}>
                     <IconComponent name={'list'} size={20} />
                 </button>
             </div>
