8b46bd473846fd725ca7d751ceac29ffeefd10db
Mahdi Tasha <imwhdiiii@gmail.com>
2023-11-15 13:20:29 +0330

    added mqs of first section of home page]

--- component/headerComponent.tsx
+++ component/headerComponent.tsx
@@ -11,19 +11,17 @@ import LinkComponent from "@/chunk/header/linkComponent";
 export default function HeaderComponent():ReactNode {
     // Returning JSX
     return (
-        <header className={'px-[32px] py-[36px] lg:flex items-center justify-between gap-[10px] flex-wrap bg-white'}>
+        <header className={'px-[32px] py-[36px] lg:flex items-center justify-between gap-[10px] flex-wrap'}>
             <div className={'flex items-center lg:justify-start justify-between lg:w-auto w-full gap-[30px]'}>
-                <button className={'lg:hidden flex aspect-square w-[50px] items-center justify-center rounded-[14px] bg-white border border-dark text-black'}>
+                <button className={'lg:hidden flex justify-center items-center bg-white border border-dark aspect-square w-[40px] text-dark rounded-[12px]'}>
                     <IconComponent name={'list-right'} size={20} />
                 </button>
                 <Link href={'/'}>
                     <Image src={LogoImage.src} alt={'لوگو وبیمود'} width={56} height={56} />
                 </Link>
-                <div className={'lg:hidden block'}>
-                    <button className={'w-[50px] aspect-square p-0 btn-primary'}>
-                        <IconComponent name={'shaking-hands'} size={20} />
-                    </button>
-                </div>
+                <button className={'lg:hidden flex justify-center items-center bg-theme theme-shadow aspect-square w-[40px] text-white rounded-[12px]'}>
+                    <IconComponent name={'shaking-hands'} size={20} />
+                </button>
                 <div className={'vertical-divider lg:block hidden'} />
                 <ul className={'lg:flex hidden items-center gap-[30px]'}>
                     <li><LinkComponent activeInHome link={'/work-samples'}>نمونه کار ها</LinkComponent></li>
@@ -37,11 +35,11 @@ export default function HeaderComponent():ReactNode {
                 </ul>
             </div>
             <div className={'lg:flex hidden gap-[20px]'}>
-                <Link href={'#'} className={'btn-primary'}>
+                <Link href={'#'} className={'btn-primary no-py'}>
                     درخواست همکاری
                     <IconComponent name={'chevron-left'} size={15} />
                 </Link>
-                <button className={'aspect-square w-[50px] flex items-center justify-center rounded-[14px] bg-white border border-dark text-black'}>
+                <button className={'flex justify-center items-center bg-white border border-dark aspect-square w-[50px] text-dark rounded-[14px]'}>
                     <IconComponent name={'list'} size={20} />
                 </button>
             </div>
--- component/page/section/home/firstSectionComponent.tsx
+++ component/page/section/home/firstSectionComponent.tsx
@@ -10,13 +10,13 @@ import bgZigzagImage from '@/public/img/home/firstSection/img-bg-zigzag.svg';
 export default function FirstSectionComponent():ReactNode {
     // Returning JSX
     return (
-        <section className={'relative pt-[100px]'}>
-            <img className={'absolute top-0 z-[-1]'} src={bgZigzagImage.src} alt="عکس زیگزاگ در پس زمینه" />
-            <div className={'flex gap-[186px] justify-between items-start px-[20px]'}>
-                <main className={'w-[50%]'}>
-                    <div className={'px-[12px] py-[8px] bg-theme theme-shadow mb-[10px] inline-block text-[15px] font-normal text-white rounded-l-[12px] rounded-tr-[12px]'}>تیم وبیمود</div>
-                    <h1 className={'text-[64px] text-dark font-bold'}>وبیمود</h1>
-                    <h2 className={'text-[64px] relative inline-block mb-[24px]'}>
+        <section className={'xl:relative xl:pt-[100px] pt-[96px]'}>
+            <img className={'absolute top-0 right-0 z-[-1]'} src={bgZigzagImage.src} alt="عکس زیگزاگ در پس زمینه" />
+            <div className={'flex xl:flex-row flex-col xl:gap-[186px] gap-[112px] justify-between items-start px-[20px]'}>
+                <main className={'xl:w-[50%] w-full xl:block flex items-center flex-col'}>
+                    <div className={'px-[12px] py-[8px] bg-theme theme-shadow xl:mb-[10px] mb-[20px] inline-block xl:text-[15px] text-[10px] font-normal text-white rounded-l-[12px] rounded-tr-[12px]'}>تیم وبیمود</div>
+                    <h1 className={'xl:block hidden text-[64px] text-dark font-bold'}>وبیمود</h1>
+                    <h2 className={'xl:inline-block hidden text-[64px] relative mb-[24px]'}>
                         <span className={'text-dark font-normal inline-block ml-[10px]'}>واقعی کردن</span>
                         <span className={'text-theme font-bold inline-block'}>ایده ها</span>
                         <span className={'text-theme absolute right-[102%] bottom-full flex flex-col w-[30px]'}>
@@ -25,20 +25,30 @@ export default function FirstSectionComponent():ReactNode {
                             <span className={'self-start'}><IconComponent name={'star'} size={18} /></span>
                         </span>
                     </h2>
-                    <p className={'text-[28px] font-normal text-lightGrey leading-[56px] mb-[58px]'}>بهترین خدمات برنامه نویسی ، طرای سایت ، طراحی لوگو ، انیمیشن و... با تیم حرفه ای وبیمود کسب و کار خود را به سطح باللاتری ببرید !</p>
-                    <div className={'flex flex-wrap gap-[34px]'}>
-                        <Link href={'tel:9120000000'} className={'btn-primary text-big'}>
-                            <IconComponent name={'telephone'} size={16} />
+                    <h1 className={'relative xl:hidden block text-[20px] font-bold mb-[8px] text-center'}>
+                        <span className={'inline-block ml-[1ch] text-dark'}>وبیمود وقعی کردن</span>
+                        <span className={'inline-block text-theme'}>ایده ها</span>
+                        <span className={'text-theme absolute right-full bottom-full flex flex-col w-[15px]'}>
+                            <span className={'self-center'}><IconComponent name={'star'} size={4} /></span>
+                            <span className={'self-end'}><IconComponent name={'star'} size={5} /></span>
+                            <span className={'self-start'}><IconComponent name={'star'} size={7} /></span>
+                        </span>
+                    </h1>
+                    <p className={'xl:text-[28px] text-[13px] xl:text-start text-center font-normal text-lightGrey xl:leading-[56px] leading-[26px] xl:mb-[58px] mb-[32px]'}>بهترین خدمات برنامه نویسی ، طرای سایت ، طراحی لوگو ، انیمیشن و... با تیم حرفه ای وبیمود کسب و کار خود را به سطح باللاتری ببرید !</p>
+                    <div className={'flex xl:flex-row flex-col xl:w-auto w-full xl:flex-wrap xl:gap-[34px] gap-[24px]'}>
+                        <Link href={'tel:9120000000'} className={'btn-primary text-big xl:w-auto w-full'}>
+                            <span className={'xl:inline hidden'}><IconComponent name={'telephone'} size={16} /></span>
+                            <span className={'xl:hidden inline'}><IconComponent name={'telephone'} size={20} /></span>
                             ارتباط با ما
                         </Link>
-                        <Link href={'/work-samples'} className={'btn-secondary text-big'}>
+                        <Link href={'/work-samples'} className={'btn-secondary text-big xl:w-auto w-full'}>
                             نمونه کار های ما
                             <IconComponent name={'chevron-left'} size={16} />
                         </Link>
                     </div>
                 </main>
-                <div className={'w-[50%]'}>
-                    <Image width={625} height={962} src={leftSideImage.src} alt={'عکس گوشی'} className={'w-full'} />
+                <div className={'xl:w-[50%] w-full'}>
+                    <Image width={625} height={962} src={leftSideImage.src} alt={'عکس گوشی'} className={'w-full xl:-mt-[100px] '} />
                 </div>
             </div>
         </section>
