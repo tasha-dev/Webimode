ed3170f67b6e0d6cc2a583500c74a16d65843994
Mahdi Tasha <imwhdiiii@gmail.com>
2023-11-19 16:02:38 +0330

    added styles of fifth section of home page

--- component/page/section/home/fifthSectionComponent.tsx
+++ component/page/section/home/fifthSectionComponent.tsx
@@ -3,16 +3,18 @@ import {ReactNode} from "react";
 import IconComponent from '@/chunk/iconComponent';
 import Image from "next/image";
 import MainImage from '@/public/img/home/fifthSection/img-main.png';
+import BgImage from '@/public/img/home/fifthSection/img-bg-overlay.png';
 
 // Creating and exorting fifth section component as default
 export default function FifthSectionComponent():ReactNode {
     // Returning JSX
     return (
-        <section className="bg-theme">
-            <div className="container px-[20px] py-[48px]">
-                <header>
+        <section className="bg-gradient-to-b from-theme to-darkerTheme relative h-[717px]">
+            <Image src={BgImage.src} width={1140} height={717} alt="عکس بکگراند" className="opacity-50 w-full h-full object-cover absolute top-0 left-0 z-[1] pointer-events-none" />
+            <div className="container px-[20px] py-[48px] z-[2]">
+                <header className="flex flex-col items-center justify-center">
                     <div className="sign-white">چرا وبیمود</div>
-                    <h4 className={'relative lg:text-[48px] text-[20px] text-center text-white'}>
+                    <h4 className={'relative lg:text-[48px] text-[20px] text-center text-white mb-[56px]'}>
                         <span className={'inline-block ml-[1ch] font-normal'}>چرا باید</span>
                         <span className={'inline-block ml-[1ch] font-bold'}>وبیمود</span>
                         <span className={'inline-block font-normal'}>را انتخاب کنید ?</span>
@@ -23,10 +25,14 @@ export default function FifthSectionComponent():ReactNode {
                         </span>
                     </h4>
                 </header>
-                <main>
-                    <h6>پشتیبانی 24/7 در کنار شما</h6>
-                    <p>تیم وبیمود افتخار این را دارد بتا بتواند در تمامی مراحل ایده پردازی تا تحویل و پشتیبانی پروژه در تمام ساعت زور و روز های هفته ر کنار شما باشد</p>
-                    <Image src={MainImage.src} alt={'ویژگی های وبیمود'} width={750} height={787} />
+                <main className="flex flex-col items-center justify-center">
+                    <h6 className="text-center mb-[4px] text-white text-[16px] font-bold">پشتیبانی 24/7 در کنار شما</h6>
+                    <p className="text-center mb-[16px] text-white text-[13px] font-normal leading-[28px]">
+                        تیم وبیمود افتخار این را دارد بتا بتواند در تمامی مراحل ایده پردازی تا 
+                        <br />
+                        تحویل و پشتیبانی پروژه در تمام ساعت زور و روز های هفته ر کنار شما باشد
+                    </p>
+                    <Image src={MainImage.src} alt={'ویژگی های وبیمود'} width={750} height={787} className="max-w-[750px]" />
                 </main>
             </div>
         </section>
