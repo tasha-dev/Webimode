416d1e8ecf669cf8c073f0bf9b263c920122d93f
Mahdi Tasha <imwhdiiii@gmail.com>
2023-11-20 17:22:39 +0330

    dded styles of seventh section of home page

--- chunk/page/home/seventhSection/featuresComponent.tsx
+++ chunk/page/home/seventhSection/featuresComponent.tsx
@@ -5,26 +5,39 @@ import {ReactNode} from "react";
 // Defining type of props
 interface propsType {
     isActive: boolean;
+    isGoingToBeActive: boolean;
     title: string;
     children: string;
     position: 'top' | 'bottom';
     icon: 'shield-check'|'hand-holding-dollor'|'shaking-hands';
+    isLastOfType?: boolean;
 }
 
 // Creating and exporting feature component as default
-export default function FeaturesComponent({children, isActive, position, title, icon}:propsType):ReactNode {
+export default function FeaturesComponent({children, isActive, position, title, icon, isLastOfType = false, isGoingToBeActive}:propsType):ReactNode {
     // Returning JSX
     return (
-        <div data-acive={isActive} data-position={position}>
-            <div>
+        <div data-active={isActive} data-position={position} className="data-[position='bottom']:mt-[120px]">
+            <div data-active={isActive} className="mx-auto w-[64px] h-[64px] aspect-square flex justify-center items-center rounded-[16px] mb-[16px] data-[active='true']:text-white data-[active='true']:bg-theme data-[active='false']:text-lightGrey data-[active='false']:bg-lighterGrey data-[active='true']:shadow-theme">
                 <IconComponent name={icon} size={32} />
             </div>
-            <span>
-                {title}
-            </span>
-            <p>
-                {children}
-            </p>
+            <span data-active={isActive} className="truncate block mb-[8px] w-full text-center data-[active='true']:text-theme data-[active='false']:text-lightGrey">{title}</span>
+            <p data-active={isActive} className="line-clamp-4 text-[12px] font-normal data-[active='false']:text-lighterGrey data-[active='true']:text-dark leading-[24px] mb-[20px]">{children}</p>
+            <div className="relative">
+                <svg data-active={isActive} className="data-[active='true']:text-theme data-[active='false']:text-lightGrey data-[active='false']:text-lightGrey w-full" viewBox="0 0 214 291" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M107.493 240.026L3.12142 183.586L107.493 129.494L211.865 183.586L107.493 240.026Z" stroke="currentColor" strokeWidth="2"/>
+                    <path d="M107.496 92.7541L48.4661 60.8334L107.496 30.2405L166.526 60.8334L107.496 92.7541Z" fill="currentColor" fillOpacity="0.16" stroke="currentColor" strokeWidth="1.5"/>
+                    <path d="M103.965 87.8038L168.125 60.5273V180.597C168.125 187.237 164.024 193.187 157.818 195.55L109.388 213.99C106.77 214.987 103.965 213.053 103.965 210.252V87.8038Z" fill="currentColor"/>
+                    <path d="M110.402 87.8014L46.862 60.7705V180.916C46.862 187.518 50.9179 193.443 57.0733 195.832L104.955 214.414C107.577 215.431 110.402 213.497 110.402 210.685L110.402 87.8014Z" fill="currentColor"/>
+                </svg>
+                {
+                    (position === 'top')
+                        ? <div data-going-active={isGoingToBeActive} data-active={isActive} className="h-[200px] w-[7px] data-[active='false']:bg-lightGrey data-[active='true']:bg-theme data-[going-active='true']:bg-gradient-to-b data-[going-active='true']:via-lightGrey data-[going-active='true']:from-theme data-[going-active='true']:to-lightGrey rotate-[60deg] absolute top-[50%] -translate-y-[4%] right-full z-[-1]" />
+                        : (isLastOfType)
+                            ? false
+                            : <div data-going-active={isGoingToBeActive} data-active={isActive} className="h-[230px] w-[7px] data-[active='false']:bg-lightGrey data-[active='true']:bg-theme data-[going-active='true']:bg-gradient-to-t data-[going-active='true']:via-lightGrey data-[going-active='true']:from-theme data-[going-active='true']:to-lightGrey rotate-[-60deg] absolute top-[50%] -translate-y-[55%] left-0 z-[-1]" />
+                }
+            </div>
         </div>
     );
 }
--- component/page/section/home/seventhSectionComponent.tsx
+++ component/page/section/home/seventhSectionComponent.tsx
@@ -1,6 +1,7 @@
 // Importing part
 import {ReactNode} from "react";
 import FeaturesComponent from "@/chunk/page/home/seventhSection/featuresComponent";
+import IconComponent from "@/chunk/iconComponent";
 
 // Creating and exporting seventh section of home page as default
 export default function SeventhSectionComponent():ReactNode {
@@ -8,28 +9,31 @@ export default function SeventhSectionComponent():ReactNode {
     return (
         <section>
             <div className="container p-[20px]">
-                <header>
+                <header className="mb-[100px]">
                     <div className="sign">امنیت وبیمود</div>
-                    <div>
+                    <div className="flex items-center justify-between gap-[100px]">
                         <div>
-                            <h4>
+                            <h4 className="text-[48px] block text-dark font-normal whitespace-nowrap relative">
                                 ما 
-                                <span>امنیت پرداختی و سرویس</span>    
+                                <span className="text-theme mr-[1ch] font-bold">امنیت پرداختی و سرویس</span>    
+                                <span className={'text-theme absolute right-[102%] bottom-full flex flex-col w-[30px]'}>
+                                    <span className={'self-center'}><IconComponent name={'star'} size={14} /></span>
+                                    <span className={'self-end'}><IconComponent name={'star'} size={14} /></span>
+                                    <span className={'self-start'}><IconComponent name={'star'} size={18} /></span>
+                                </span>
                             </h4>
-                            <h4>
-                                شما را تضمین میکنیم.
-                            </h4>
+                            <h4 className="text-[48px] whitespace-nowrap block text-dark font-normal">شما را تضمین میکنیم.</h4>
                         </div>
-                        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنانلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان .</p>
+                        <p className="text-[20px] text-lightGrey font-normal leading-[36px]">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنانلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان .</p>
                     </div>
                 </header>
-                <main>
-                    <FeaturesComponent icon="shield-check" isActive={true} position="top" title="خرید امن با وبیمود">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</FeaturesComponent>
-                    <FeaturesComponent icon="hand-holding-dollor" isActive={true} position="top" title="مرکز حل اختلافات با وبیمود">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</FeaturesComponent>
-                    <FeaturesComponent icon="shaking-hands" isActive={true} position="top" title="ضمانت بازگشت وجه با وبیمود">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</FeaturesComponent>
-                    <FeaturesComponent icon="shaking-hands" isActive={true} position="top" title="ضمانت بازگشت وجه با وبیمود">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</FeaturesComponent>
-                    <FeaturesComponent icon="hand-holding-dollor" isActive={true} position="top" title="ضمانت بازگشت وجه با وبیمود">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</FeaturesComponent>
-                    <FeaturesComponent icon="shaking-hands" isActive={true} position="top" title="ضمانت بازگشت وجه با وبیمود">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</FeaturesComponent>
+                <main className="grid grid-cols-6 gap-[20px]">
+                    <FeaturesComponent icon="shield-check" isActive={true} isGoingToBeActive={false} position="top" title="خرید امن با وبیمود">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</FeaturesComponent>
+                    <FeaturesComponent icon="hand-holding-dollor" isActive={true} isGoingToBeActive={true} position="bottom" title="مرکز حل اختلافات با وبیمود">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</FeaturesComponent>
+                    <FeaturesComponent icon="shaking-hands" isActive={false} isGoingToBeActive={false} position="top" title="ضمانت بازگشت وجه با وبیمود">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</FeaturesComponent>
+                    <FeaturesComponent icon="shaking-hands" isActive={false} isGoingToBeActive={false} position="bottom" title="ضمانت بازگشت وجه با وبیمود">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</FeaturesComponent>
+                    <FeaturesComponent icon="hand-holding-dollor" isActive={false} isGoingToBeActive={false} position="top" title="ضمانت بازگشت وجه با وبیمود">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</FeaturesComponent>
+                    <FeaturesComponent isLastOfType icon="shaking-hands" isActive={false} isGoingToBeActive={false} position="bottom" title="ضمانت بازگشت وجه با وبیمود">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</FeaturesComponent>
                 </main>
             </div>
         </section>
