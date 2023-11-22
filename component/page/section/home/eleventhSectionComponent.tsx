59fb47e055c9e8af4b956493f6708f9518bc7af2
Mahdi Tasha <imwhdiiii@gmail.com>
2023-11-22 14:19:27 +0330

    added mqs of last section of home page

--- component/page/section/home/eleventhSectionComponent.tsx
+++ component/page/section/home/eleventhSectionComponent.tsx
@@ -7,11 +7,11 @@ import {ReactNode} from "react";
 export default function EleventhSectionComponent():ReactNode {
     // Returning JSX
     return (
-        <section className="mt-[196px] w-[50%] ml-[150px]">
+        <section className="lg:mt-[196px] mt-[156px] lg:w-[50%] lg:ml-[150px]">
             <div className="container p-[20px]">
-                <header className="mb-[56px]">
+                <header className="lg:mb-[56px] mb-[24px] lg:block flex items-center justify-center flex-col">
                     <div className="sign">ارتباط با ما</div>
-                    <h4 className={'relative lg:text-[48px] text-[20px] font-bold mb-[8px] text-start'}>
+                    <h4 className={'relative lg:text-[48px] text-[20px] font-bold mb-[8px] lg:text-start text-center'}>
                         <span className={'inline-block ml-[1ch] text-dark'}>به راحتی با تیم ما</span>
                         <span className={'inline-block text-theme'}>ارتباط بگیر !</span>
                         <span className={'text-theme absolute right-full bottom-full lg:flex hidden flex-col w-[30px]'}>
@@ -20,28 +20,30 @@ export default function EleventhSectionComponent():ReactNode {
                             <span className={'self-start'}><IconComponent name={'star'} size={18} /></span>
                         </span>
                     </h4>
-                    <p className="text-lightGrey font-normal text-[20px] leading-[36px]">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است .</p>
+                    <p className="text-lightGrey font-normal lg:text-[20px] text-[13px] lg:leading-[36px] leading-[28px] lg:text-start text-center">
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است .
+                    </p>
                 </header>
                 <main>
-                    <div className="flex gap-[32px] items-center flex-wrap mb-[32px]">
-                        <Link className="flex gap-[16px] items-center group" href='tel:0912999999999'>
-                            <div className="border border-theme bg-white transition-all duration-500 flex justify-center items-center text-theme w-[56px] h-[56px] aspect-square rounded-full group-hover:bg-theme group-hover:text-white">
+                    <div className="flex lg:flex-row flex-col lg:gap-[32px] gap-[24px] items-center flex-wrap lg:mb-[32px] mb-[24px]">
+                        <Link className="flex lg:flex-row flex-col lg:gap-[16px] gap-[12px] items-center group" href='tel:0912999999999'>
+                            <div className="border border-theme bg-white transition-all duration-500 shadow-none flex justify-center items-center text-theme w-[56px] h-[56px] aspect-square rounded-full group-hover:bg-theme group-hover:text-white group-hover:shadow-lg">
                                 <IconComponent size={16} name="telephone" />
-                                </div>
-                            <span className="text-[20px] font-normal text-theme">0912999999999</span>
+                            </div>
+                            <span className="text-[20px] font-normal text-theme lg:text-start text-center">0912999999999</span>
                         </Link>
-                        <Link className="flex gap-[16px] items-center group" href='mailto:webimode@yahoo.com'>
-                            <div className="border border-theme bg-white transition-all duration-500 flex justify-center items-center text-theme w-[56px] h-[56px] aspect-square rounded-full group-hover:bg-theme group-hover:text-white">
+                        <Link className="flex lg:flex-row flex-col lg:gap-[16px] gap-[12px] items-center group" href='mailto:webimode@yahoo.com'>
+                            <div className="border border-theme bg-white transition-all duration-500 shadow-none flex justify-center items-center text-theme w-[56px] h-[56px] aspect-square rounded-full group-hover:bg-theme group-hover:text-white group-hover:shadow-lg">
                                 <IconComponent size={16} name="mail" />
-                                </div>
-                            <span className="text-[20px] font-normal text-theme">webimode@yahoo.com</span>
+                            </div>
+                            <span className="text-[20px] font-normal text-theme lg:text-start text-center">webimode@yahoo.com</span>
                         </Link>
                     </div>
-                    <div className="flex gap-[16px] items-center group">
-                        <div className="border border-theme bg-white transition-all duration-500 flex justify-center items-center text-theme w-[56px] h-[56px] aspect-square rounded-full group-hover:bg-theme group-hover:text-white">
+                    <div className="flex lg:flex-row flex-col lg:gap-[16px] gap-[12px] items-center group">
+                        <div className="border border-theme bg-white transition-all duration-500 shadow-none flex justify-center items-center text-theme w-[56px] h-[56px] aspect-square rounded-full group-hover:bg-theme group-hover:text-white group-hover:shadow-lg">
                             <IconComponent size={16} name="location" />
                         </div>
-                        <p className="text-[20px] font-normal text-theme">تهران ، میدان انقلاب ، کوچه سوم ، ساختمان الماس ، هلدینگ وبیمود ، طبقه سوم ، درب اول</p>
+                        <p className="text-[20px] font-normal text-theme lg:text-start text-center">تهران ، میدان انقلاب ، کوچه سوم ، ساختمان الماس ، هلدینگ وبیمود ، طبقه سوم ، درب اول</p>
                     </div>
                 </main>
             </div>
