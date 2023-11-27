7a6b69040cef88f5f28ac04e2f43b574e26fa5a6
Mahdi Tasha <imwhdiiii@gmail.com>
2023-11-27 18:13:02 +0330

    added mqs of first section of blog page

--- chunk/page/blog/firstSection/searchFormComponent.tsx
+++ chunk/page/blog/firstSection/searchFormComponent.tsx
@@ -18,7 +18,7 @@ export default function SearchFormComponent({ name, placeHolder }: propsType):Re
 
     // Returninng JSX
     return (
-        <form action='#' data-focused={isFocused} className="w-[50%] mb-[64px] transition-all duration-500 flex items-center justify-between rounded-[18px] border-[1.5px] data-[focused='false']:border-lightGrey data-[focused='true']:border-dark overflow-hidden">
+        <form action='#' data-focused={isFocused} className="lg:w-[50%] w-full transition-all duration-500 flex items-center justify-between rounded-[18px] border-[1.5px] data-[focused='false']:border-lightGrey data-[focused='true']:border-dark overflow-hidden">
             <input onFocus={() => setFocused(true)} onBlur={() => setFocused(false)} required type="text" data-focused={isFocused} placeholder={placeHolder} name={name} id={name} className="p-[16px] w-full transition-all duration-500 outline-0 text-[16px] data-[focused='true']:text-dark data-[focused='false']:text-lightGrey font-normal" />
             <button data-focused={isFocused} className="p-[20px] relative before:transition-all before:duration-500 before:content-[''] before:absolute before:right-0 before:top-[50%] before:translate-y-[-50%] before:w-[1px] transition-all duration-500 before:rounded-[2px] before:h-[20px] data-[focused='true']:before:bg-dark data-[focused='false']:before:bg-lightGrey data-[focused='true']:text-dark data-[focused='false']:text-lightGrey focus:bg-lighterGrey outline-0 focus:text-lightGrey">
                 <IconComponent size={16} name="search" />
--- component/page/section/blog/firstSectionComponent.tsx
+++ component/page/section/blog/firstSectionComponent.tsx
@@ -8,8 +8,8 @@ export default function FirstSectionComponent():ReactNode {
     // Returning JSX
     return (
         <section className="mb-[1000px]">
-            <div className="container p-[20px] lg:mt-[250px] mt-[68px] relative">
-                <div className="absolute top-0 left-0 w-full h-full -translate-y-[20%] pointer-events-none">
+            <div className="container lg:mt-[250px] mt-[68px] relative">
+                <div className="absolute lg:block hidden top-0 left-0 w-full h-full -translate-y-[20%] pointer-events-none p-[20px]">
                     <div className="absolute top-0 right-[200px] w-[8px] h-[8px] aspect-square bg-theme rounded-full" />
                     <div className="flex items-start gap-[5px] absolute top-[10px] left-[300px] text-themePurple">
                         <div className="w-[16px] h-[16px] aspect-square rounded-full bg-current" />
@@ -23,13 +23,13 @@ export default function FirstSectionComponent():ReactNode {
                     </div>
                     <div className="absolute bottom-[100px] left-[100px] w-[8px] h-[8px] aspect-square bg-theme rounded-full" />
                 </div>
-                <header className="flex items-center justify-center flex-col mb-[48px]">
+                <header className="flex items-center justify-center flex-col lg:mb-[48px] mb-[16px] p-[20px]">
                     <div className="sign">مقالات وبیمود</div>
-                    <h1 className="font-normal text-[48px] text-dark mb-[16px]">
+                    <h1 className="font-normal lg:text-[48px] text-[20px] text-dark mb-[16px] text-center">
                         جدیدترین و داغ ترین 
                         <span className="relative font-bold mx-[1ch] text-theme inline-block">
                             مقالات
-                            <span className={'text-theme absolute right-[102%] bottom-full flex flex-col w-[30px]'}>
+                            <span className={'text-theme absolute right-[102%] bottom-full lg:flex hidden flex-col w-[30px]'}>
                                 <span className={'self-center'}><IconComponent name={'star'} size={14} /></span>
                                 <span className={'self-end'}><IconComponent name={'star'} size={14} /></span>
                                 <span className={'self-start'}><IconComponent name={'star'} size={18} /></span>
@@ -39,17 +39,19 @@ export default function FirstSectionComponent():ReactNode {
                     </h1>
                     <p className="text-center paragraph">در مقالات وبیمود می توانید جدید ترین و بهترین مقالاتی که تا به حال ندیده اید را مشاهده کنید و از آن لذت ببرید !</p>
                 </header>
-                <main className="flex items-center justify-center flex-col">
-                    <SearchFormComponent name="blog-search-input" placeHolder="اسم مقاله ، دسته بندی ، ...."  />
-                    <ul className="flex items-center gap-[32px] justify-between overflow-auto w-[75%] no-scroll">
-                        <li className="shrink-0"><button data-active={true} className="blog-genre-btn">همه مقالات</button></li>
-                        <li className="shrink-0"><button data-active={false} className="blog-genre-btn">تکنولوژی</button></li>
-                        <li className="shrink-0"><button data-active={false} className="blog-genre-btn">سلامت و بهداشت</button></li>
-                        <li className="shrink-0"><button data-active={false} className="blog-genre-btn">ایده پردازی</button></li>
-                        <li className="shrink-0"><button data-active={false} className="blog-genre-btn">هوش مصنوعی</button></li>
-                        <li className="shrink-0"><button data-active={false} className="blog-genre-btn">جشنواره ها</button></li>
-                        <li className="shrink-0"><button data-active={false} className="blog-genre-btn">طراحی و خلاقیت</button></li>
-                    </ul>
+                <main>
+                    <div className="px-[20px] w-full flex items-center justify-center flex-col lg:mb-[64px] mb-[32px]">
+                        <SearchFormComponent name="blog-search-input" placeHolder="اسم مقاله ، دسته بندی ، ...."  />
+                    </div>
+                    <ul className="flex items-center gap-[32px] justify-between overflow-auto max-w-[1000px] no-scroll mx-auto px-[20px]">
+                            <li className="shrink-0"><button data-active={true} className="blog-genre-btn">همه مقالات</button></li>
+                            <li className="shrink-0"><button data-active={false} className="blog-genre-btn">تکنولوژی</button></li>
+                            <li className="shrink-0"><button data-active={false} className="blog-genre-btn">سلامت و بهداشت</button></li>
+                            <li className="shrink-0"><button data-active={false} className="blog-genre-btn">ایده پردازی</button></li>
+                            <li className="shrink-0"><button data-active={false} className="blog-genre-btn">هوش مصنوعی</button></li>
+                            <li className="shrink-0"><button data-active={false} className="blog-genre-btn">جشنواره ها</button></li>
+                            <li className="shrink-0"><button data-active={false} className="blog-genre-btn">طراحی و خلاقیت</button></li>
+                        </ul>
                 </main>
             </div>
         </section>
