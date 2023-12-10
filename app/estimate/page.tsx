f7aac47d23994eb0655f622df584d5c5b3f5f7dd
Mahdi Tasha <imwhdiiii@gmail.com>
2023-12-10 16:21:58 +0330

    added second section of estiamte page

--- app/estimate/page.tsx
+++ app/estimate/page.tsx
@@ -1,6 +1,7 @@
 // Importing part
 import {ReactNode} from "react";
 import FirstSectionComponent from "@/component/page/section/estimate/firstSectionComponent";
+import SecondSectionComponent from "@/component/page/section/estimate/secondSectionComponent";
 
 // Creating and exporting estimate page as default
 export default function EstimatePage():ReactNode {
@@ -8,6 +9,7 @@ export default function EstimatePage():ReactNode {
     return (
         <>
             <FirstSectionComponent />
+            <SecondSectionComponent />
         </>
     );
 }
