25c8c4087779b9e5411004bf364259e77cde9e04
Mahdi Tasha <imwhdiiii@gmail.com>
2023-12-11 19:57:34 +0330

    small bugss

--- component/page/section/chat-gpt-app/middleSideComponent.tsx
+++ component/page/section/chat-gpt-app/middleSideComponent.tsx
@@ -7,13 +7,13 @@ export default function MiddleSideComponent():ReactNode {
     // Returning JSX
     return (
         <div className="col-span-3 bg-dark h-[100vh] grid grid-cols-1 grid-rows-6">
-            <div className="overflow-x-auto overflow-y-hidden flex gap-[24px] items-center justify-center row-span-1 p-[32px]">
+            <div className="overflow-x-auto overflow-y-hidden flex gap-[24px] items-center justify-center row-span-1 p-[32px] no-scroll">
                 <button data-active={true} className="chat-ai-btn">babbage</button>
                 <button data-active={false} className="chat-ai-btn">davinci</button>
                 <button data-active={false} className="chat-ai-btn">GPT4</button>
                 <button data-active={false} className="chat-ai-btn">midjourny</button>
             </div>
-            <div className="flex items-center justify-center col-span-1 row-span-4 overflow-x-hidden overflow-y-auto p-[32px]">
+            <div className="flex items-center justify-center col-span-1 row-span-4 overflow-x-hidden overflow-y-auto p-[32px] no-scroll">
                 <div className="p-[24px] w-[75%] shadow-black border border-lightestDark bg-pageDark rounded-[40px]">
                     <div className="flex w-[50%] items-center mx-auto p-[16px] bg-white/20 rounded-[12px] justify-between gap-[10px] shadow-black mb-[32px]">
                         <span className="text-lightGrey shrink-0"><IconComponent name="circle-warn" size={16} /></span>
@@ -33,8 +33,8 @@ export default function MiddleSideComponent():ReactNode {
                     </ul>
                 </div>
             </div>
-            <div className="col-span-1 row-span-1 p-[32px]">
-                <form action="#" className="bg-pageDark border border-lightestDark rounded-[12px] shadow-black flex items-center overflow-hidden" dir="ltr">
+            <div className="col-span-1 row-span-1 p-[32px] flex flex-col items-center justify-center">
+                <form action="#" className="bg-pageDark border border-lightestDark rounded-[12px] shadow-black flex w-full items-center overflow-hidden" dir="ltr">
                     <input placeholder="چیزی تایپ کنید ...." type="text" className="px-[16px] py-[12px] w-full bg-transparent text-[20px] font-normal outline-0 placeholder:text-white/50 text-white" />
                     <button className="p-[10px] shrink-0 aspect-square w-[50px] flex items-center justify-center">
                         <span className="rotate-180 text-lightGrey"><IconComponent size={24} name="send" /></span>
