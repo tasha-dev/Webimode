02659683a7fb37ae65f3aabb87750054205b1b46
Mahdi Tasha <imwhdiiii@gmail.com>
2023-12-11 17:59:31 +0330

    added file of chat gpt app page

---
+++ app/chat-gpt/app/page.tsx
@@ -0,0 +1,12 @@
+// Importing part
+import {ReactNode} from "react";
+
+// Creating and exporting chat gpt app page as default
+export default function ChatGptAppPage():ReactNode {
+    // Returning JSX
+    return (
+        <>
+            asdasd
+        </>
+    );
+}
