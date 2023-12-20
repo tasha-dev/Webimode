b1de9677adf9765fc6a8d70271a5a241c21fd871
Mahdi Tasha <imwhdiiii@gmail.com>
2023-12-20 14:13:21 +0330

    added last page

---
+++ app/dashboard/faq/page.tsx
@@ -0,0 +1,33 @@
+// Importing part
+import {ReactNode} from "react";
+import DashboardPageComponent from "@/component/dashboard/dashboardPageTemplate";
+import RandomImage from '@/public/img/blog/img-random.png';
+import Image from "next/image";
+
+// Creating and exporting Faq page of dashboard as default
+export default function DashboardFaqPage():ReactNode {
+    // Returning JSX
+    return (
+        <DashboardPageComponent>
+            <span className="lg:text-[20px] text-[16px] font-normal text-dark block lg:mb-[8px] mb-[12px]">این متن امتحانی هست</span>
+            <p className="lg:mb-[32px] mb-[16px] text-lightGrey lg:text-[16px] text-[13px] lg:leading-[32px] leading-[28px] font-normal">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
+            <div className="grid lg:grid-cols-4 gap-[20px] mb-[32px]">
+                <Image className="lg:col-span-2 object-cover h-[220px] rounded-[32px]" src={RandomImage.src} alt="" width={1000} height={1000} />
+                <Image className="col-span-1 object-cover h-[220px] rounded-[32px]" src={RandomImage.src} alt="" width={1000} height={1000} />
+                <Image className="col-span-1 object-cover h-[220px] rounded-[32px]" src={RandomImage.src} alt="" width={1000} height={1000} />
+            </div>
+            <span className="lg:text-[20px] text-[16px] font-normal text-dark block lg:mb-[8px] mb-[12px]">این متن امتحانی هست</span>
+            <p className="lg:mb-[32px] mb-[16px] text-lightGrey lg:text-[16px] text-[13px] lg:leading-[32px] leading-[28px] font-normal">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
+            <div className="grid lg:grid-cols-3 gap-[20px] mb-[32px]">
+                <Image className="lg:col-span-1 object-cover h-[220px] rounded-[32px]" src={RandomImage.src} alt="" width={1000} height={1000} />
+                <Image className="col-span-2 object-cover h-[220px] rounded-[32px]" src={RandomImage.src} alt="" width={1000} height={1000} />
+            </div>
+            <span className="lg:text-[20px] text-[16px] font-normal text-dark block lg:mb-[8px] mb-[12px]">این متن امتحانی هست</span>
+            <p className="lg:mb-[32px] mb-[16px] text-lightGrey lg:text-[16px] text-[13px] lg:leading-[32px] leading-[28px] font-normal">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
+            <div className="grid lg:grid-cols-3 gap-[20px]">
+                <Image className="col-span-1 object-cover rounded-[32px] h-full" src={RandomImage.src} alt="" width={1000} height={1000} />
+                <p className="lg:col-span-2 text-lightGrey lg:text-[16px] text-[13px] lg:leading-[32px] leading-[28px] font-normal">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است و چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
+            </div>
+        </DashboardPageComponent>
+    );
+}
