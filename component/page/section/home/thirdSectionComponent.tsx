00dda586889c54c47b07bca71acfb7a65292f116
Mahdi Tasha <imwhdiiii@gmail.com>
2023-12-30 14:06:28 +0330

    changed border-radius of left side when hovered and added buttons and hoveraility to service components

--- component/page/section/home/thirdSectionComponent.tsx
+++ component/page/section/home/thirdSectionComponent.tsx
@@ -55,7 +55,7 @@ export default function ThirdSectionComponent():ReactNode {
                     </div>
                 </header>
                 <main className='xl:grid hidden grid-cols-2 gap-[20px]'>
-                <ServiceItemComponent icon='design' iconGradiant='blue' img={FigmaImage.src} hasButtons hasHoverAnimation title='طراحی UI UX'>بهترین طراحی طراحی و تجربه کاربری در زمینه وب سایت و اپلیکیشن با مدرن ترین متود های روز دنیا</ServiceItemComponent>
+                <ServiceItemComponent icon='design' iconGradiant='blue' img={FigmaImage.src} title='طراحی UI UX'>بهترین طراحی طراحی و تجربه کاربری در زمینه وب سایت و اپلیکیشن با مدرن ترین متود های روز دنیا</ServiceItemComponent>
                 <ServiceItemComponent icon='window' iconGradiant='purple' img={CodeImage.src} title='برنامه نویسی وب سایت'>بهترین طراحی طراحی و تجربه کاربری در زمینه وب سایت و اپلیکیشن با مدرن ترین متود های روز دنیا</ServiceItemComponent>
                 <ServiceItemComponent icon='poll' iconGradiant='green' img={ChartsImage.src} title='تولید محتوا'>بهترین طراحی طراحی و تجربه کاربری در زمینه وب سایت و اپلیکیشن با مدرن ترین متود های روز دنیا</ServiceItemComponent>
                 <ServiceItemComponent icon='pencil-paintbrush' iconGradiant='orange' img={PencilImage.src} title='طراحی لوگو'>بهترین طراحی طراحی و تجربه کاربری در زمینه وب سایت و اپلیکیشن با مدرن ترین متود های روز دنیا</ServiceItemComponent>
--- component/serviceItemComponent.tsx
+++ component/serviceItemComponent.tsx
@@ -15,54 +15,46 @@ interface propsType {
     img: string;
     title: string;
     children: string;
-    hasHoverAnimation?: boolean;
-    hasButtons?: boolean;
 }
 
 // Creating and exporting service items as default
-export default function ServiceItemComponent({children, icon, img, title, hasButtons = false, hasHoverAnimation = false, iconGradiant}:propsType):ReactNode {
+export default function ServiceItemComponent({children, icon, img, title, iconGradiant}:propsType):ReactNode {
     // Returning JSX
     return (
-        <div className="lg:h-[528px] h-[555px] overflow-hidden rounded-[40px] relative group">
-            <div className={`lg:absolute relative top-0 left-0 w-full transition-all ease-in-out duration-1000 z-[-1] ${(hasHoverAnimation) ? 'group-hover-skew' : ''}`}>
-                <div className="h-full w-full scale-110 rounded-[40px] bg-black/50 absolute top-0 left-0 z-[2]"></div>
+        <div className="lg:h-[528px] h-[555px] rounded-r-[40px] transition-all ease-in-out duration-1000 rounded-l-[40px] hover:rounded-l-[0px] overflow-hidden relative group">
+            <div className={'lg:absolute relative top-0 left-0 w-full transition-all ease-in-out duration-1000 z-[-1] group-hover-skew'}>
+                <div className="h-full w-full scale-110 rounded-[40px] bg-black/50 absolute top-0 left-0 z-[2]" />
                 <Image className="w-full scale-110 rounded-[40px] object-cover lg:h-full h-[555px]" src={img} alt={title} width={1000} height={528} />
             </div>
-            <div className={`transition-all ease-in-out duration-1000 h-full w-full absolute top-0 left-0 p-[44px] flex items-end justify-start origin-right ${(hasHoverAnimation) ? 'group-hover-skew-small-scale' : ''}`}>
+            <div className={'transition-all ease-in-out duration-1000 h-full w-full absolute top-0 left-0 p-[44px] flex items-end justify-start origin-right group-hover-skew-small-scale'}>
                 <div className="lg:w-[75%] w-full">
-                    <div data-gradiant={iconGradiant} className="bg-gradient-to-br w-[56px] h-[56px] aspect-auto flex justify-center items-center mb-[12px] rounded-[12px] text-white data-[gradiant='blue']:from-lightTheme data-[gradiant='blue']:to-themeBlue data-[gradiant='green']:from-frogGreen data-[gradiant='green']:to-theme data-[gradiant='purple']:from-themePurple data-[gradiant='purple']:to-themeBlue data-[gradiant='orange']:from-lightOrange data-[gradiant='orange']:to-orange data-[gradiant='deep-green']:from-darkFrogGreen data-[gradiant='deep-green']:to-frogGreen data-[gradiant='pink']:from-pink data-[gradiant='pink']:to-themePurple data-[gradiant='reversed-pink']:from-themePurple data-[gradiant='reversed-pink']:to-pink data-[gradiant='dark-blue']:from-themeBlue data-[gradiant='dark-blue']:to-lightTheme lg:mx-0 mx-auto">
+                    <div 
+                        data-gradiant={iconGradiant} 
+                        className="bg-gradient-to-br w-[56px] h-[56px] aspect-auto flex justify-center items-center mb-[12px] rounded-[12px] text-white data-[gradiant='blue']:from-lightTheme data-[gradiant='blue']:to-themeBlue data-[gradiant='green']:from-frogGreen data-[gradiant='green']:to-theme data-[gradiant='purple']:from-themePurple data-[gradiant='purple']:to-themeBlue data-[gradiant='orange']:from-lightOrange data-[gradiant='orange']:to-orange data-[gradiant='deep-green']:from-darkFrogGreen data-[gradiant='deep-green']:to-frogGreen data-[gradiant='pink']:from-pink data-[gradiant='pink']:to-themePurple data-[gradiant='reversed-pink']:from-themePurple data-[gradiant='reversed-pink']:to-pink data-[gradiant='dark-blue']:from-themeBlue data-[gradiant='dark-blue']:to-lightTheme lg:mx-0 mx-auto"
+                    >
                         <IconComponent name={icon} size={32} />
                     </div>
                     <span className="block truncate text-white text-[24px] font-semibold mb-[4px] lg:text-start text-center">{title}</span>
                     <p className="text-white text-[16px] font-normal line-clamp-2 lg:text-start text-center">{children}</p>
-                    {
-                        (hasButtons)
-                            ? (
-                                <div className={'flex lg:flex-row flex-col mt-[40px] lg:w-auto w-full lg:flex-wrap gap-[12px]'}>
-                                    <Link href={'/contact-us'} className={'btn-primary-white lg:w-auto w-full'}>
-                                        ثبت سفارش
-                                        <IconComponent name='chevron-left' size={16} />
-                                    </Link>
-                                    <Link href={'/work-samples'} className={'btn-secondary-white lg:w-auto w-full'}>
-                                        نمونه کار های ما
-                                        <IconComponent name={'chevron-left'} size={16} />
-                                    </Link>
-                                </div>
-                            ) : false
-                    }
+                    <div className={'flex lg:flex-row flex-col mt-[40px] lg:w-auto w-full lg:flex-wrap gap-[12px]'}>
+                        <Link href={'/contact-us'} className={'btn-primary-white lg:w-auto w-full'}>
+                            ثبت سفارش
+                            <IconComponent name='chevron-left' size={16} />
+                        </Link>
+                        <Link href={'/work-samples'} className={'btn-secondary-white lg:w-auto w-full'}>
+                            نمونه کار های ما
+                            <IconComponent name={'chevron-left'} size={16} />
+                        </Link>
+                    </div>
                 </div>
             </div>
-            {
-                (hasHoverAnimation)
-                    ? <Image 
-                        alt="نمونه کار" 
-                        src={RandomImage.src} 
-                        width={1000} 
-                        height={1000} 
-                        className="absolute w-[50%] top-0 left-0 h-full origin-left object-cover rounded-[20px] opacity-0 transition-all ease-in-out duration-1000 group-hover-inner-img lg:group-hover:opacity-100" 
-                    />
-                    : false
-            }
+            <Image
+                alt="نمونه کار"
+                src={RandomImage.src}
+                width={1000}
+                height={1000}
+                className="absolute w-[50%] top-0 left-0 h-full origin-left object-cover rounded-[20px] opacity-0 transition-all ease-in-out duration-1000 group-hover-inner-img lg:group-hover:opacity-100"
+            />
         </div>
     );
 }
