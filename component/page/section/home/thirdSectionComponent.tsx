648a1d3f70b295d2f91de449a48eacc0cb166768
Mahdi Tasha <imwhdiiii@gmail.com>
2023-12-30 14:33:06 +0330

    fixed type error in seventh section of home page

--- component/page/section/home/thirdSectionComponent.tsx
+++ component/page/section/home/thirdSectionComponent.tsx
@@ -76,7 +76,7 @@ export default function ThirdSectionComponent():ReactNode {
                             prevEl: '#prev-services-slider-btn',
                         }}
                     >
-                        <SwiperSlide><ServiceItemComponent icon='design' iconGradiant='blue' img={FigmaImage.src} hasButtons hasHoverAnimation title='طراحی UI UX'>بهترین طراحی طراحی و تجربه کاربری در زمینه وب سایت و اپلیکیشن با مدرن ترین متود های روز دنیا</ServiceItemComponent></SwiperSlide>
+                        <SwiperSlide><ServiceItemComponent icon='design' iconGradiant='blue' img={FigmaImage.src} title='طراحی UI UX'>بهترین طراحی طراحی و تجربه کاربری در زمینه وب سایت و اپلیکیشن با مدرن ترین متود های روز دنیا</ServiceItemComponent></SwiperSlide>
                         <SwiperSlide><ServiceItemComponent icon='window' iconGradiant='purple' img={CodeImage.src} title='برنامه نویسی وب سایت'>بهترین طراحی طراحی و تجربه کاربری در زمینه وب سایت و اپلیکیشن با مدرن ترین متود های روز دنیا</ServiceItemComponent></SwiperSlide>
                         <SwiperSlide><ServiceItemComponent icon='poll' iconGradiant='green' img={ChartsImage.src} title='تولید محتوا'>بهترین طراحی طراحی و تجربه کاربری در زمینه وب سایت و اپلیکیشن با مدرن ترین متود های روز دنیا</ServiceItemComponent></SwiperSlide>
                         <SwiperSlide><ServiceItemComponent icon='pencil-paintbrush' iconGradiant='orange' img={PencilImage.src} title='طراحی لوگو'>بهترین طراحی طراحی و تجربه کاربری در زمینه وب سایت و اپلیکیشن با مدرن ترین متود های روز دنیا</ServiceItemComponent></SwiperSlide>
