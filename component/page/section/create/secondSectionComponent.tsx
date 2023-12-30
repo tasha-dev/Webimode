232c07319feb5212928be0637b943fd86142a838
Mahdi Tasha <imwhdiiii@gmail.com>
2023-12-30 12:02:44 +0330

    made items in second section of create page to be active by default in mq

--- component/page/section/create/secondSectionComponent.tsx
+++ component/page/section/create/secondSectionComponent.tsx
@@ -9,7 +9,7 @@ export default function SecondSectionComponent():ReactNode {
         <section className="lg:mt-[calc(208px-20px)] mt-[40px] lg:mb-[225px] mb-[40px]">
             <div className="container p-[20px]">
                 <main className="grid lg:grid-cols-4 grid-cols-1 gap-[20px]">
-                    <FeaturesComponent isActive title="ثبت رایگان پروژه" icon="rounded-star" link="#">به رایگان و در چند کلیک پروژه خود را ثبت و از متخصصین مختلف پیشنهاد قیمت و زمان طراحی سایت مورد نظرتان را دریافت نمایید.</FeaturesComponent>
+                    <FeaturesComponent title="ثبت رایگان پروژه" icon="rounded-star" link="#">به رایگان و در چند کلیک پروژه خود را ثبت و از متخصصین مختلف پیشنهاد قیمت و زمان طراحی سایت مورد نظرتان را دریافت نمایید.</FeaturesComponent>
                     <FeaturesComponent title="پشتیبانی لحظه ای و مستمر" icon="headphone" link="#">به رایگان و در چند کلیک پروژه خود را ثبت و از متخصصین مختلف پیشنهاد قیمت و زمان طراحی سایت مورد نظرتان را دریافت نمایید.</FeaturesComponent>
                     <FeaturesComponent title="بازگشت 2% برای هر پروژه" icon="percentage" link="#">به رایگان و در چند کلیک پروژه خود را ثبت و از متخصصین مختلف پیشنهاد قیمت و زمان طراحی سایت مورد نظرتان را دریافت نمایید.</FeaturesComponent>
                     <FeaturesComponent title="گارانتی حسن انجام کار" icon="shield-check" link="#">به رایگان و در چند کلیک پروژه خود را ثبت و از متخصصین مختلف پیشنهاد قیمت و زمان طراحی سایت مورد نظرتان را دریافت نمایید.</FeaturesComponent>
