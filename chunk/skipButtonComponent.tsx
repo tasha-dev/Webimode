091bdf9c3767abe3fef9c31c0ca749094d14b967
Mahdi Tasha <imwhdiiii@gmail.com>
2024-01-09 15:19:59 +0330

    fixed small bug in next section which hast sticky scroll things

--- chunk/skipButtonComponent.tsx
+++ chunk/skipButtonComponent.tsx
@@ -12,7 +12,7 @@ interface propsType {
 export default function SkipButtonComponent({id, marginTop = 250}:propsType):ReactNode {
     // Returning JSX
     return (
-        <div className={`lg:flex hidden items-center justify-center mt-[${marginTop}px]`}>
+        <div className={`lg:flex hidden items-center justify-center`} style={{marginTop: `${marginTop}px`}}>
             <Link 
                 href={id} 
                 className="px-[16px] py-[8px] rounded-[12px] text-lightGrey font-normal text-[15px] transition-all duration-500 bg-lightestGrey hover:bg-theme hover:text-white"
