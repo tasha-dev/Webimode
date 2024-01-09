9f21e67650ccc0305263f51238f381ab711af7a9
Mahdi Tasha <imwhdiiii@gmail.com>
2024-01-09 15:38:49 +0330

    change possition of rectangle pulse animation in blog pages to prevent them to collide to the title of next section of them

--- chunk/animation/rectanglePulseComponent.tsx
+++ chunk/animation/rectanglePulseComponent.tsx
@@ -5,7 +5,7 @@ import {ReactNode} from "react";
 export default function ReactagnlePulseComponent():ReactNode {
     // Returning JSX
     return (
-        <div className="absolute lg:top-[85%] top-[100%] left-[50%] -translate-x-[50%] -translate-y-[50%] lg:w-[75%] w-[80%] lg:h-[356px] h-[100px] z-[-1]">
+        <div className="absolute lg:top-[80%] top-[100%] left-[50%] -translate-x-[50%] -translate-y-[50%] lg:w-[75%] w-[80%] lg:h-[356px] h-[100px] z-[-1]">
             <div className="absolute top-0 left-0 w-full h-full scale-0 opacity-0 border border-dark rounded-[50px] animate-rectanglePusle animation-delay-4" />
             <div className="absolute top-[50%] left-[50%] scale-0 opacity-0 w-[80%] h-[80%] border border-dark rounded-[50px] translate-x-[-50%] translate-y-[-50%] animate-rectanglePusleCentred animation-delay-3" />
             <div className="absolute top-[50%] left-[50%] scale-0 opacity-0 w-[60%] h-[60%] border border-dark rounded-[50px] translate-x-[-50%] translate-y-[-50%] animate-rectanglePusleCentred animation-delay-2" />
