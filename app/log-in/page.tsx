2f28f94eee2c5eaaaa72fbf7bcd07b86d4013216
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-05 17:17:47 +0330

    handled form of login page

--- app/log-in/page.tsx
+++ app/log-in/page.tsx
@@ -1,4 +1,7 @@
-// Improrting part
+// Forcing nextJS to render this component as client side component
+'use client';
+
+// importing part
 import {ReactNode} from 'react';
 import Link from 'next/link';
 import IconComponent from '@/chunk/iconComponent';
@@ -6,9 +9,37 @@ import InputComponent from '@/chunk/inputComponent';
 import HalfDividerComponent from '@/chunk/halfDividerComponent';
 import Image from 'next/image';
 import LeftSideImage from '@/public/img/img-log-in.jpg';
+import {z} from 'zod';
+import {zodResolver} from "@hookform/resolvers/zod";
+import {SubmitHandler, useForm} from "react-hook-form";
+
+// Defining type of form
+const formSchema = z.object({
+  email: z.string().email('لطفا ایمیل را به درستی وارد کنید.'),
+  password: z.string().min(8, 'رمز عبور نمیتواند کمتر از 8 کارکتر باشد.').max(12, 'رمز عبور نمیتواند بیشتر از 12 کارکتر باشد.')
+});
+
+type formType = z.infer<typeof formSchema>;
 
 // Creating and exporting Log in page as default
 export default function LogInPage():ReactNode {
+  // Defining useForm hook to handle form
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors,
+      isValidating
+    }
+  } = useForm<formType>({
+    resolver: zodResolver(formSchema)
+  });
+
+  // Handling submit event of form
+  const submitEventHandler: SubmitHandler<formType> = (data) => {
+    console.log(data);
+  }
+
   // Returning JSX 
   return (
     <section className='lg:mb-0 mb-[50px]'>
@@ -29,10 +60,10 @@ export default function LogInPage():ReactNode {
               <p className="paragraph lg:text-start text-center">وارد حساب کاربریتان شوید و از روند پروژه هایتان مطلع شوید و با متخصصان ما در مورد روند پروژه صحبت کنید</p>
             </header>
             <main>
-              <form action="#" className="grid lg:grid-cols-2 grid-cols-1 gap-[20px] mb-[32px]">
-                <InputComponent name="name-email-input" required placeHolder="نام کاربری یا ایمیل ...." inputType="text" icon="person" />
-                <InputComponent icon='slash-eye' name="password-input" required placeHolder="رمز عبور ...." inputType="password" />
-                <button className="lg:col-span-2 col-span-1 mt-[40px] btn-secondary-theme">
+              <form onSubmit={handleSubmit(submitEventHandler)} action="#" className="grid lg:grid-cols-2 grid-cols-1 gap-[20px] mb-[32px]">
+                <InputComponent inputType={'email'} errorText={errors.email?.message} register={register} name="email" placeHolder="ایمیل ...." icon="person" />
+                <InputComponent inputType={'password'} errorText={errors.password?.message} register={register} icon='slash-eye' name="password" placeHolder="رمز عبور ...." />
+                <button disabled={isValidating} className="lg:col-span-2 col-span-1 mt-[40px] btn-secondary-theme">
                     ورود به حساب
                   <IconComponent size={16} name="chevron-left" />   
                 </button>
--- chunk/inputComponent.tsx
+++ chunk/inputComponent.tsx
@@ -4,43 +4,84 @@
 // Importing part
 import {ReactNode, useState, Dispatch} from 'react';
 import IconComponent from '@/chunk/iconComponent';
+import {UseFormRegister} from "react-hook-form";
 
 // Defining type of props
 interface propsType {
-  placeHolder: string;
-  name: string;
-  icon: 'person'|'telephone'|'caller'|'mail'|'slash-eye';
-  inputType: 'text'|'email'|'password'|'tel';
-  required?: boolean;
+    placeHolder: string;
+    name: string;
+    icon: 'person' | 'telephone' | 'caller' | 'mail' | 'slash-eye';
+    inputType: 'text' | 'email' | 'password' | 'tel';
+    errorText: string | undefined;
+    register: UseFormRegister<any>;
 }
 
 // Creating and exporting input component as default
-export default function InputComponent({placeHolder, name, icon, inputType, required = false}:propsType):ReactNode {
-  // Defining state of component
-  const [isFocused, setFoucused]:[boolean, Dispatch<boolean>] = useState(false);
+export default function InputComponent({placeHolder, name,icon, inputType, errorText, register}: propsType): ReactNode {
+    // Defining state of component
+    const [isFocused, setFoucused]: [boolean, Dispatch<boolean>] = useState(false);
 
-  // Conditional rendering
-  if (inputType === 'password') {
-    // Defining state of component 
-    const [isPasswordShowing, setPasswordShowing]:[boolean, Dispatch<boolean>] = useState(false);
+    // Conditional rendering
+    if (inputType === 'password') {
+        // Defining state of component
+        const [isPasswordShowing, setPasswordShowing]: [boolean, Dispatch<boolean>] = useState(false);
 
-    // Returning JSX
-    return (
-      <div data-focused={isFocused} className="rounded-[16px] overflow-hidden flex justify-between items-center border-2 transition-all duration-500 data-[focused='true']:border-dark data-[focused='false']:border-lightGrey">
-        <input placeholder={placeHolder} name={name} id={name} onFocus={() => setFoucused(true)} onBlur={() => setFoucused(false)} data-focused={isFocused} type={(isPasswordShowing) ? 'text' : 'password'} required={required} maxLength={12} minLength={8} className="p-[16px] text-[16px] font-normal w-full outline-0 transition-all duration-500 data-[focused='true']:text-dark data-[focused='false']:text-lightGrey"  />
-        <button type={'button'} onClick={() => (isPasswordShowing) ? setPasswordShowing(false) : setPasswordShowing(true)} data-focused={isFocused} className="p-[20px] data-[focused='true']:text-dark data-[focused='false']:text-lightGrey transition-all duration-500 hover:bg-lighterGrey focus:bg-lighterGrey outline-0">
-          <IconComponent size={16} name={icon} />
-        </button>
-      </div>
-    );
-  } else {
-    return (
-      <div data-focused={isFocused} className="rounded-[16px] overflow-hidden flex justify-between items-center border-2 transition-all duration-500 data-[focused='true']:border-dark data-[focused='false']:border-lightGrey">
-        <input data-type={inputType} placeholder={placeHolder} name={name} id={name} onFocus={() => setFoucused(true)} onBlur={() => setFoucused(false)} data-focused={isFocused} type={inputType} required={required} className="p-[16px] text-[16px] font-normal w-full outline-0 transition-all duration-500 data-[focused='true']:text-dark data-[focused='false']:text-lightGrey data-[type='tel']:text-end" />
-        <div data-focused={isFocused} className="p-[16px] data-[focused='true']:text-dark data-[focused='false']:text-lightGrey transition-all duration-500">
-          <IconComponent size={16} name={icon} />
-        </div>
-      </div>
-    );
-  }
+        // Returning JSX
+        return (
+            <div>
+                <div data-focused={isFocused}
+                     className="rounded-[16px] overflow-hidden flex justify-between items-center border-2 transition-all duration-500 data-[focused='true']:border-dark data-[focused='false']:border-lightGrey">
+                    <input
+                        {...register(name, {
+                            onBlur: () => setFoucused(false)
+                        })}
+                        placeholder={placeHolder}
+                        onFocus={() => setFoucused(true)}
+                        data-focused={isFocused}
+                        type={(isPasswordShowing) ? 'text' : 'password'}
+                        className="p-[16px] text-[16px] font-normal w-full outline-0 transition-all duration-500 data-[focused='true']:text-dark data-[focused='false']:text-lightGrey"
+                    />
+                    <button
+                        type={'button'}
+                        onClick={() => (isPasswordShowing) ? setPasswordShowing(false) : setPasswordShowing(true)}
+                        data-focused={isFocused}
+                        className="p-[20px] data-[focused='true']:text-dark data-[focused='false']:text-lightGrey transition-all duration-500 hover:bg-lighterGrey focus:bg-lighterGrey outline-0"
+                    >
+                        <IconComponent size={16} name={icon}/>
+                    </button>
+                </div>
+                {
+                    (errorText)
+                        ? <p className={'mt-[20px] text-[15px] font-normal text-red-600'}>{errorText}</p>
+                        : false
+                }
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <div data-focused={isFocused} className="rounded-[16px] overflow-hidden flex justify-between items-center border-2 transition-all duration-500 data-[focused='true']:border-dark data-[focused='false']:border-lightGrey">
+                    <input
+                        {...register(name, {
+                            onBlur: () => setFoucused(false)
+                        })}
+                        placeholder={placeHolder}
+                        onFocus={() => setFoucused(true)}
+                        data-focused={isFocused}
+                        className="p-[16px] text-[16px] font-normal w-full outline-0 transition-all duration-500 data-[focused='true']:text-dark data-[focused='false']:text-lightGrey data-[type='tel']:text-end"
+                    />
+                    <div data-focused={isFocused}
+                         className="p-[16px] data-[focused='true']:text-dark data-[focused='false']:text-lightGrey transition-all duration-500"
+                    >
+                        <IconComponent size={16} name={icon}/>
+                    </div>
+                </div>
+                {
+                    (errorText)
+                        ? <p className={'mt-[20px] text-[15px] font-normal text-red-600'}>{errorText}</p>
+                        : false
+                }
+            </div>
+        );
+    }
 }
