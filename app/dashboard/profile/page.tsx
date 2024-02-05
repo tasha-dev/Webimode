4f894a0a02a17a87799ae57e52f60c8b7c401291
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-05 19:46:00 +0330

    added validation to profile page in dashboard

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,21 +1,63 @@
+// Forcing nextJS to render this component as client side component
+'use client';
+
 // Importing part
 import {ReactNode} from "react";
 import DashboardPageTemplate from "@/component/dashboard/dashboardPageTemplate";
 import EditInfoInputComponent from '@/chunk/page/dashboard/editInfoInputComponent';
 import Link from "next/link";
 import IconComponent from "@/chunk/iconComponent";
+import {z} from "zod";
+import {SubmitHandler, useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+// Defining type of form
+const formSchema = z.object({
+    name: z.string().min(2, 'نام کمتر از 2 کارکتر میباشد.').max(100, 'نام بیشتر از 100 کارکتر میباشد.'),
+    tel: z.string().min(11, 'شماره تلفن از 11 عدد کمتر نباید باشد.').max(11, 'شماره تلفن از 11 عدد بیشتر نباید باشد.'),
+    email: z.string().email('لطفا ایمیل را به درستی وارد کنید.'),
+    meli: z.string().min(10, 'کد ملی نمیتواند کمتر از 10 کارکتر باشد.').max(12, 'کد ملی نمیتواند بیشتر از 10 کارکتر باشد.'),
+    password: z.string().min(8, 'رمز عبور نمیتواند کمتر از 8 کارکتر باشد.').max(12, 'رمز عبور نمیتواند بیشتر از 12 کارکتر باشد.')
+});
+
+type formType = z.infer<typeof formSchema>;
 
 // Creating and exporting profile dashboard page as default
 export default function DashboardProfilePage():ReactNode {
+    // Defining useForm hook to use later
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            isValidating,
+            errors
+        }
+    } = useForm<formType>({
+        resolver: zodResolver(formSchema)
+    });
+
+    // Defining a function to handle onSubmit event of form
+    const onSubmitHandler:SubmitHandler<formType> = (data) => {
+        console.log(data);
+    }
+
     // Returning JSX
     return (
         <DashboardPageTemplate>
-            <form action="#" className="grid lg:grid-cols-4 gap-[23px]">
-                <EditInfoInputComponent id="name-input" label="نام و نام خانوادگی :" placeHolder="احسان امانیان" type="text" />
-                <EditInfoInputComponent id="tel-input" label="شماره تلفن :" placeHolder="09216542162" type="tel" />
-                <EditInfoInputComponent id="email-input" label="ایمیل :" placeHolder="dasnasldj@yahoo.com" type="email" />
-                <EditInfoInputComponent id="meli-code-input" label="کد ملی :" placeHolder="09216542162" type="text" />
-                <EditInfoInputComponent id="password-input" label="رمز عبور :" placeHolder="XXXXXXXXX" type="passowrd" />
+            <form onSubmit={handleSubmit(onSubmitHandler)} action="#" className="grid lg:grid-cols-4 gap-[23px]">
+                <EditInfoInputComponent register={register} errorText={errors.name?.message} name="name" label="نام و نام خانوادگی :" placeHolder="احسان امانیان" type="text" />
+                <EditInfoInputComponent register={register} errorText={errors.tel?.message} name="tel" label="شماره تلفن :" placeHolder="09216542162" type="tel" />
+                <EditInfoInputComponent register={register} errorText={errors.email?.message} name="email" label="ایمیل :" placeHolder="dasnasldj@yahoo.com" type="email" />
+                <EditInfoInputComponent register={register} errorText={errors.meli?.message} name="meli" label="کد ملی :" placeHolder="09216542162" type="text" />
+                <EditInfoInputComponent register={register} errorText={errors.password?.message} name="password" label="رمز عبور :" placeHolder="XXXXXXXXX" type="passowrd" />
+                {
+                    (errors.root?.message)
+                        ? (
+                            <div className={'lg:col-span-4 col-span-1'}>
+                                <p className={'text-[16px] font-normal text-red-600 mt-[16px]'}>{errors.root?.message}</p>
+                            </div>
+                        ) : false
+                }
                 <div className="lg:col-span-3 col-span-1 flex lg:flex lg:flex-row flex-col items-end justify-end gap-[16px]">
                     <Link href='/' className="btn-secondary-alert lg:w-auto w-full">
                         <IconComponent name="circle-x" size={16} />
--- chunk/page/dashboard/editInfoInputComponent.tsx
+++ chunk/page/dashboard/editInfoInputComponent.tsx
@@ -4,26 +4,34 @@
 import IconComponent from "@/chunk/iconComponent";
 // Importing part
 import {Dispatch, ReactNode, useState} from "react";
+import {UseFormRegister} from "react-hook-form";
 
 // Defining type of props
 interface propsType {
     label: string;
-    id: string;
+    name: string;
     type: 'text' | 'tel' | 'email' | 'passowrd';
     placeHolder: string;
+    register: UseFormRegister<any>;
+    errorText: string | undefined;
 }
 
 // Creating and exporting edit info input component as defualt
-export default function EditInfoInputComponent({id, label, type, placeHolder}:propsType):ReactNode {
+export default function EditInfoInputComponent({register, errorText, label, type, placeHolder, name}:propsType):ReactNode {
     // Conditional rendering
     if (type !== 'passowrd') {
         return (
             <div>
-                <label className="mb-[12px] block text-[16px] font-normal truncate dark:text-white text-dark" htmlFor={id}>{label}</label>
+                <label className="mb-[12px] block text-[16px] font-normal truncate dark:text-white text-dark">{label}</label>
                 {
-                    (id === 'meli-code-input')
-                        ? <input minLength={10} maxLength={10} className="appearance-none dark:bg-lightestDark bg-lightestGrey border dark:border-white/20 border-lighterGrey transition-all duration-500 text-right rounded-[12px] placeholder:transition-all placeholder:duration-500 outline-0 w-full block p-[12px] text-[13px] font-normal placeholder:text-lightGrey text-lightGrey focus:placeholder:text-dark focus:text-dark dark:focus:border-theme focus:border-theme" required type={type} placeholder={placeHolder} id={id} name={id} />
-                        : <input className="appearance-none bg-lightestGrey border dark:bg-lightestDark dark:border-white/20 border-lighterGrey transition-all duration-500 text-right rounded-[12px] placeholder:transition-all placeholder:duration-500 outline-0 w-full block p-[12px] text-[13px] font-normal placeholder:text-lightGrey text-lightGrey dark:focus:placeholder:text-white dark:focus:text-white focus:placeholder:text-dark focus:text-dark dark:focus:border-theme focus:border-theme" required type={type} placeholder={placeHolder} id={id} name={id} />
+                    (name === 'meli')
+                        ? <input {...register(name)} className="appearance-none dark:bg-lightestDark bg-lightestGrey border dark:border-white/20 border-lighterGrey transition-all duration-500 text-right rounded-[12px] placeholder:transition-all placeholder:duration-500 outline-0 w-full block p-[12px] text-[13px] font-normal placeholder:text-lightGrey text-lightGrey focus:placeholder:text-dark focus:text-dark dark:focus:border-theme focus:border-theme" placeholder={placeHolder} />
+                        : <input {...register(name)} className="appearance-none bg-lightestGrey border dark:bg-lightestDark dark:border-white/20 border-lighterGrey transition-all duration-500 text-right rounded-[12px] placeholder:transition-all placeholder:duration-500 outline-0 w-full block p-[12px] text-[13px] font-normal placeholder:text-lightGrey text-lightGrey dark:focus:placeholder:text-white dark:focus:text-white focus:placeholder:text-dark focus:text-dark dark:focus:border-theme focus:border-theme" placeholder={placeHolder} />
+                }
+                {
+                    (errorText)
+                        ? <p className={'text-[16px] font-normal text-red-600 mt-[16px]'}>{errorText}</p>
+                        : false
                 }
             </div>
         );
@@ -35,17 +43,24 @@ export default function EditInfoInputComponent({id, label, type, placeHolder}:pr
         // Returning JSX
         return (
             <div>
-                <label className="mb-[12px] block text-[16px] font-normal truncate text-dark" htmlFor={id}>{label}</label>
+                <label className="mb-[12px] block text-[16px] font-normal truncate text-dark">{label}</label>
                 <div data-focused={isFocused} className="overflow-hidden rounded-[12px] flex gap-[10px] dark:bg-lightestDark dark:border-white/20 bg-lightestGrey border data-[focused='false']:border-lighterGrey data-[focused='true']:border-theme transition-all duration-500">
-                    <input 
+                    <input
+                        {...register(name, {
+                            onBlur: () => setFocused(false)
+                        })}
                         onFocus={() => setFocused(true)}
-                        onBlur={() => setFocused(false)}
-                        className="p-[12px] dark:placeholder:text-lightGrey dark:text-lightGrey w-full outline-0 bg-transparent" required type={inputType} placeholder={placeHolder} id={id} name={id} minLength={8} maxLength={10} 
+                        className="p-[12px] dark:placeholder:text-lightGrey dark:text-lightGrey w-full outline-0 bg-transparent" placeholder={placeHolder}
                     />
                     <button type="button" onClick={() => (inputType === 'password') ? setInputType('text') : setInputType('password')} className="shrink-0 aspect-square w-[50px] transition-all duration-500 dark:bg-lightestDark bg-lightestGrey text-lightGrey hover:bg-lighterGrey text-[13px] font-normal focus:bg-lighterGrey flex items-center justify-center dark:focus:text-white focus:text-dark">
                         <IconComponent name={'slash-eye'} size={16} />
                     </button>
                 </div>
+                {
+                    (errorText)
+                        ? <p className={'text-[16px] font-normal text-red-600 mt-[16px]'}>{errorText}</p>
+                        : false
+                }
             </div>            
         );
     }
